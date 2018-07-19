60afca0f3ab9d512975a4693016a2175b4bfb459
ADMIN-PC\ADMIN <adamzulkarnain98@gmail.com>
2018-07-19 20:37:48 +0700

    tes

--- models/TabelArtikelSearch.php
+++ models/TabelArtikelSearch.php
@@ -5,7 +5,7 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\Models\TabelArtikel;
+use app\models\TabelArtikel;
 
 /**
  * TabelArtikelSearch represents the model behind the search form about `app\Models\TabelArtikel`.
