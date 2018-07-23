41bf53180e9dea924af35c59a5042b44ba654af3
ADMIN-PC\ADMIN <adamzulkarnain98@gmail.com>
2018-07-24 04:08:03 +0700

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=app_tiroid',
+    'dsn' => 'mysql:host=localhost;dbname=tiroid_app',
     'username' => 'root',
     'password' => 'cumlaude2018',
     'charset' => 'utf8',
